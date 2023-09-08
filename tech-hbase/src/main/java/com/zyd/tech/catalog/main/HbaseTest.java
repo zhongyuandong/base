c2b90cd659d39e00a99e4ddbcf6d3b41673900b8
zhongyuandong <zhongyuandong@zj.tech>
2023-09-08 16:05:11 +0800

    feat:hbase

--- tech-hbase/src/main/java/com/zyd/tech/catalog/main/HbaseTest.java
+++ tech-hbase/src/main/java/com/zyd/tech/catalog/main/HbaseTest.java
@@ -1,14 +1,19 @@
 package com.zyd.tech.catalog.main;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.*;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @program: base
@@ -29,13 +34,26 @@ public class HbaseTest {
             if (avTableBatch == null) {
                 avTableBatch = connection.getBufferedMutator(TableName.valueOf("k8s:wr_table_1"));
             }
-            Put putAv = new Put(Bytes.toBytes("rowkey1_" + i));
+            Put putAv = new Put(Bytes.toBytes("rowkey33_" + i));
             putAv.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("name" + 1), Bytes.toBytes("value" + i));
             staleAvToHbaseList.add(putAv);
         }
         if (CollectionUtils.isNotEmpty(staleAvToHbaseList)){
             avTableBatch.mutate(staleAvToHbaseList);
             avTableBatch.flush();
+        }
+
+
+        Table avTableQuery = connection.getTable(TableName.valueOf("iot_sz:av_detail"));
+
+        Get get = new Get(Bytes.toBytes("0000000251_axisVarietyId1_1207yc428143_1638890523"));
+        Result result = avTableQuery.get(get);
+        Map<String, String> axisMap = new HashMap<>();
+        for (Cell kv : result.rawCells()) {
+            String colName = Bytes.toString(kv.getQualifierArray(), kv.getQualifierOffset(), kv.getQualifierLength());
+            String value = Bytes.toString(kv.getValueArray(), kv.getValueOffset(), kv.getValueLength());
+            System.out.println("获取到hbase数据colName：" + colName);
+            System.out.println("获取到hbase数据value：" + value);
         }
     }
 
